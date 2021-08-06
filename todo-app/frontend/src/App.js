b358d9afbbf2df9c0d1e502cb612ff3fff8806c1
alexanderschaer <schaeralexander@gmail.com>
2021-08-06 16:44:15 +0200

    post-ToDo works

--- todo-app/frontend/src/App.js
+++ todo-app/frontend/src/App.js
@@ -1,12 +1,16 @@
-
 import './App.css';
 import Header from "./components /Header";
 import InputNewToDo from "./components /InputNewToDo";
 import Kanban from "./components /Kanban";
 import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
-  const title = "Kanban Board"
+    const title = "Kanban Board"
+
+
+    const [toDos, setToDos] = useState([])
+    const [inputText, setInputText] = useState("")
 
     useEffect(() => {
         fetch("/api/todo")
@@ -14,36 +18,48 @@ function App() {
             .then((toDoArray) => setToDos(toDoArray))
             .catch((error) => console.error(error));
     }, []);
+    useEffect(() => {
+        console.log("Marking stuff: ", toDos)
+    }, [toDos])
 
-    const [toDos, setToDos] = useState([])
-    const [inputText, setInputText] = useState("")
 
     const handleNewToDo = (event) => {
-      console.log("new item erstellen: ", inputText)
+        console.log("new item erstellen: ", inputText)
+        axios.post('/api/todo', {
+            description: inputText
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        console.log("new ToDo posted" , inputText)
     }
+
     const saveInput = (event) => {
-      setInputText(event.target.value)
-      console.log("Value: ", event.target.value)
+        setInputText(event.target.value)
+        console.log("Value: ", event.target.value)
     }
     console.log("toDos", toDos)
     return (
-    <div>
-        <header>
-            <Header title={title}/>
-        </header>
+        <div>
+            <header>
+                <Header title={title}/>
+            </header>
 
-        <main>
-            <div className="app__body">
-                <InputNewToDo handleNewToDo={handleNewToDo} inputText={inputText} saveInput={saveInput}/>
-            </div>
-            <Kanban toDos={toDos}/>
-        </main>
+            <main>
+                <div className="app__body">
+                    <InputNewToDo handleNewToDo={handleNewToDo} inputText={inputText} saveInput={saveInput}/>
+                </div>
+                <Kanban toDos={toDos}/>
+            </main>
 
-    <footer>
-        <p>Impressum</p>
-    </footer>
-    </div>
-  );
+            <footer>
+                <p>Impressum</p>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
