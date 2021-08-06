77fe661163d8b8ee38d50d034da9484b7c7217a5
lindatrosken <linda.troesken@gmx.net>
2021-08-06 15:18:25 +0200

    input handling

--- todo-app/frontend/src/App.js
+++ todo-app/frontend/src/App.js
@@ -3,24 +3,32 @@ import './App.css';
 import Header from "./components /Header";
 import InputNewToDo from "./components /InputNewToDo";
 import Kanban from "./components /Kanban";
+import {useState} from "react";
 
 function App() {
   const title = "Kanban Board"
+    const [inputText, setInputText] = useState("")
+
+    const handleNewToDo = (event) => {
+      console.log("new item erstellen: ", inputText)
+    }
+    const saveInput = (event) => {
+      setInputText(event.target.value)
+      console.log("Value: ", event.target.value)
+    }
+
   return (
     <div>
         <header>
             <Header title={title}/>
         </header>
 
-
-
         <main>
             <div className="app__body">
-                <InputNewToDo />
+                <InputNewToDo handleNewToDo={handleNewToDo} inputText={inputText} saveInput={saveInput}/>
             </div>
             <Kanban/>
         </main>
-
 
     <footer>
         <p>Impressum</p>
--- todo-app/frontend/src/components /InputNewToDo.js
+++ todo-app/frontend/src/components /InputNewToDo.js
@@ -2,7 +2,9 @@ import'./InputNewToDo.css'
 export default function InputNewToDo(props){
     return(
         <div className="input">
-            <input type="text" placeholder="new ToDo"/>
+            <input type="text" placeholder="new ToDo" value={props.inputText}
+                   onChange={props.saveInput}/>
+            <button onClick={props.handleNewToDo}>create new todo</button>
         </div>
     );
 
